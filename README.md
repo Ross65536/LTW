e34b33c98c68619a63c39c7796e94abd9da7e8f0
Ros <liandtow@gmail.com>
2017-12-14 00:45:00 +0000

    populate v2

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-# LTW
-## Trabalho desenvolvido na cadeira de LTW
+## Project developed in the LTW class for the MIEIC course at FEUP, Porto
 
 * Igor Bernardo Amorim Silveira - 201505172
 * Rostyslav Khoptiy - 201506219
 * Nuno Manuel Ferreira Corte Real - 201405158
 
-### Status
-## List Checks
+### Install instructions
 
-- [x] List all list by a given username
-- [x] Create new List
-- [x] Add Item to a list
-- [x] Mark an item as completed
-- [x] Delete list
+execute in project root:
+
+./install.sh
+
+to install a clean project.
+
+./test_install.sh
+
+to install a clean project with a few test user already inserted in the database.
+
+#### Test users
+
+user test and test3 already share a list with some items
+
+u: test
+p: Test1$00
+
+u: test2
+p: Test2$00
+
+u: test3
+p: AasP1)3ase
--- database/populate.sql
+++ database/populate.sql
@@ -1,7 +1,20 @@
-INSERT INTO users (username, password, name, email) VALUES ('test', '$2y$10$2l7VoYVm2MUBetimJSZyXOuNj9mbl7iWa7KajQxIrM6XqMu.lbtW.', 'John Doe', 'test@gmail.com'); 
+-- need to update password if passwording salting is changed
+
+INSERT INTO users (username, password, name, email, photo_url) VALUES ('test', '$2y$10$2l7VoYVm2MUBetimJSZyXOuNj9mbl7iWa7KajQxIrM6XqMu.lbtW.', 'John Doe', 'test@gmail.com', 0); 
 -- u: test
 -- pass: Test1$00
 
-INSERT INTO users (username, password, name, email) VALUES ('test2', '$2y$10$iu8nh01z.nGjyPBdsUyooO65lohUg8NpF3MBmq//azTD0U0uy40si', 'Meery Dones', 'test2@fe.up.pt'); 
+INSERT INTO users (username, password, name, email, photo_url) VALUES ('test2', '$2y$10$iu8nh01z.nGjyPBdsUyooO65lohUg8NpF3MBmq//azTD0U0uy40si', 'Meery Dones', 'test2@fe.up.pt', 0); 
 -- u: test2
 -- pass: Test2$00
+
+INSERT INTO users (username, password, photo_url) VALUES ('test3', '$2y$10$A/yQcYYJUUgACsv8wt6p8eU7L6y/7XlmFWn2QsGg0kpPpF80p9WXG', 0); 
+-- u: test3
+-- pass: AasP1)3ase
+
+-- list: test list
+INSERT INTO lists (date_created, name, creator, list_image) VALUES (date('now'), 'test list', 'test3', 0);
+INSERT INTO list_users (list_id, username) VALUES (1, 'test');
+INSERT INTO list_items (list_id, description, done) VALUES (1, 'item 1', 0);
+INSERT INTO list_items (list_id, description, done) VALUES (1, 'item 2', 0);
+INSERT INTO list_items (list_id, description, done) VALUES (1, 'item 3', 0);
